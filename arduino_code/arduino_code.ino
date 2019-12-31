b9ef602291a8f5bd3cee14e127ed79c196d8bf21
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-12-31 12:01:18 +1100

    Dual Motor Support

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,12 +1,15 @@
 #include <AFMotor.h>
 
 AF_DCMotor motor3(3);
+AF_DCMotor motor2(2);
 void setup() {
   // put your setup code here, to run once:
   motor3.setSpeed(255);
+  motor2.setSpeed(255);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   motor3.run(FORWARD);
+  motor2.run(FORWARD);
 }
